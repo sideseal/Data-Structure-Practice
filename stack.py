15e4ac8b3d47928f1d1fa030261740c38986f68e
Typie <ckgun@naver.com>
2021-05-24 16:33:45 +0900

    2021.05.24

--- stack.py
+++ stack.py
@@ -1,22 +1,41 @@
 class stack(list):
     def __init__(self):
         self.stack = []
-    
+        self.size = 0
+
     def push(self, data):
         self.stack.append(data)
-    
+        self.size += 1
+
+    def find(self, data):
+        for i in range(self.size):
+            if self.stack[i] == data:
+                return i
+        return False
+
     def pop(self):
         if self.is_empty():
             return -1
         return self.stack.pop()
-    
+
     def peek(self):
         return self.stack[-1]
-    
+
     def is_empty(self):
         if len(self.stack) == 0:
             return True
         return False
+
+    def clear(self):
+        self.stack.clear()
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        return f"[{', '.join(list(map(str, self.stack)))}]"
+
 
 if __name__ == "__main__":
     s = stack()
