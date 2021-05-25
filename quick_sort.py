e89d669ce249189d54ffb0e6f05836571cee9ab4
Choi GyeongGeon <ckgun@naver.com>
2021-05-26 00:17:03 +0900

    2021.05.26

---
+++ quick_sort.py
@@ -0,0 +1,26 @@
+def partition(arr, start, end):
+    pivot = arr[start]
+    left = start + 1
+    right = end
+    done = False
+    while not done:
+        while left <= right and arr[left] <= pivot:
+            left += 1
+        while left <= right and pivot <= arr[right]:
+            right -= 1
+        if right < left:
+            done = True
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+        arr[start], arr[right] = arr[right], arr[start]
+    return right
+
+def quick_sort(arr,start,end):
+    if start < end:
+        pivot = partition(arr, start, end)
+        quick_sort(arr, start, pivot-1)
+        quick_sort(arr,pivot+1,end)
+    return arr
+
+arr = [6,1,3,5,4,7,9,8,2]
+print(quick_sort(arr,0,len(arr)-1))
